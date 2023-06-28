13046ceb7f6aa7ab3ccdaf3dd7aa2080f340333e
nozilla_hoo <13952448030hoo@gmail.com>
2023-06-28 10:53:17 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -6,57 +6,43 @@ from flask import url_for
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 
-#操作系统判断
-WIN = sys.platform.startswith('win')
-if WIN: #如果是windows系统，使用3个斜线
-    prefix = 'sqlite:///'
-else:   #否则使用四个斜线
-    prefix = 'sqlite:////'
-
 #实例化对象
 app = Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+
+# 操作系统判断
+# WIN = sys.platform.startswith('win')
+# if WIN: #如果是windows系统，使用3个斜线
+#     prefix = 'sqlite:///'
+# else:   #否则使用四个斜线
+#     prefix = 'sqlite:////'
+# app.config['SQLAlCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
 #在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
 #变量传值
-name = 'nozilla'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# name = 'nozilla'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
 
 #路由注册
 @app.route('/')
 def index():
     user = User.query.first() #读取用户记录
     movies = Movie.query.all() #读取所有电影记录
-    return render_template('index.html',user=user,movie=movies)
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+    return render_template('index.html',user=user,movies=movies)
 
 #建表操作
 class User(db.Model): #表名将会是 user（自动生成，小写处理）
@@ -70,10 +56,18 @@ class Movie(db.Model): #表名将会是movie
 
 #自定义initdb
 @ app.cli.command() #注册为命令，可以传入name参数来自定义命令
+@click.option('--drop',is_flag=True,help='Create after drop.') #设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop: #判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') #输出提示信息
+
+@ app.cli.command()
 def forge():
     """Generate fake data."""
     db.create_all()
-
     #全局的两个变量移动到这个函数内
     name = 'nozilla'
     movies = [
@@ -97,10 +91,3 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
-@click.option('--drop',is_flag=True,help='Create after drop.') #设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop: #判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.') #输出提示信息
